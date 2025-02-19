72ba3f45da7561386729644123b623bca9753b4c
Estrelliita <estrellalr216@gmail.com>
2025-02-19 10:53:14 -0700

    prueba

--- minershell.c
+++ minershell.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 
 #define MAX_INPUT_SIZE 1024
-#define MAX_TOKEN_SIZE 64
+#define MAX_TOKEN_SIZE 32
 #define MAX_NUM_TOKENS 64
 
 /*Split the string by space and return the array of tokens**/
