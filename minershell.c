4c72d8b7da2064d8fbcf27f311a63d996de5f911
Estrella Lara <estrellalr216@gmail.com>
2025-02-26 12:05:57 -0700

    draft of pipeline and redirection logic

--- minershell.c
+++ minershell.c
@@ -7,6 +7,7 @@
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
+#define MAX_COMMANDS 10 //Max number of commands in a pipeline for now
 
 /*Split the string by space and return the array of tokens**/
 
@@ -137,35 +138,155 @@ void f_exit(char **tokens){
   exit(0);
 }
 
+void execute_pipeline(char *line){
+
+  //1. Parse into pipelines
+  char *commands[MAX_COMMANDS];
+  int num_commands = 0;
+  char *token; //pointer to next token
+  char *rest = line; //pointer to the remaining line
+
+  while((token = strtok_r(rest, '|', &rest))){
+    commands[num_commands++] = strdup(token);
+  }
+  int pipes[num_commands -1][2]; /*[num_c -1] because there will be one less pippe than the number of commands, and [2] because there are 2 file descriptors per pipe (read & write)*/
+
+
+  //2. Loop through pipeline stages
+  for(i = 0; i < num_commands; i++){
+    //3. Create pipes except for last command
+
+    //Last command, no pipe created
+    if((i<num_commands-1) && pipe(pipes[i] == -1)){
+      exit(1);
+    }
+  
+    //4. Fork
+    pid_t pid = fork();
+    if(pid == 0){
+      char **tokens = tokenize(commands[i]);
+
+      //Redirection logic
+      char *in_file = NULL;
+      char *out_file = NULL;
+
+      for(int j=0; tokens[j] != NULL; j++){
+	if(strcmp(tokens[j],"<") == 0){
+	  in_file = tokens[j+1];
+	  tokens[j] = NULL;
+	  break;
+	}else if(strcmp(tokens[j], ">") == 0){
+	  out_file = tokens[j+1];
+	  tokens[j] = NULL;
+	  break;
+	}
+      }
+      if(in_file != NULL){
+	int fd = open(in_file, ORDONLY);
+	if(fd == -1){ //throw error if input file can't be opened
+	  perror("Failure to open input file");
+	  exit(1);
+	}
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+      }
+      if(out_file != NULL){
+	int fd = open(out_file, O_WRONLY | O_CREAT, 0644);
+	if(fd == -1){
+	  perror("Failed to open output file");
+	  exit(1);
+	}
+	dup2(fd, STDOUT_FILENO);
+	dup2(fd, STDERR_FILENO);
+	close(fd);
+      }
+
+      //Pipe redirection logic
+      if(i>0){ //If this is not the first command
+	dup2(pipes[i-1][0], STDIN_FILENO); //Read from previous pipe
+	close(pipes[i-1][1]; //close write end
+      }
+      if(i<num_commands -1){//if this is not the last command
+	dup2(pipes[i][1], STDOUT_FILENO);//write to next pipe
+	close(pipes[i][0]; //close read end
+      }
+
+      //Close all other pipe ends
+      for (int j = 0; j < num_commands -1; j++){
+	if(j!=i-1 && j != i){
+	  close(pipes[j][0]);
+	  close(piepes[j][1]);
+	}
+      }
+
+	//Execute command
+	execvp(tokens[0], tokens);
+	exit(0);
+
+      }else if(pid >0){//Parent process
+	//Close unused pipe ends
+	if(i>0){
+	  close(pipes[i-1][0]);
+	  close(pipes[i-1][1]);
+	}
+	if(i==num_commands-1){
+	  for(int j = 0; j<num_commands-1;j++){
+	    close(pipes[j][0]);
+	    close(pipes[j][1]);
+	  }
+	}
+	if(i==num_commands-1){
+	  for(int j = 0; j <num_commands;j++){
+	    wait(NULL);}
+	}
+      }
+
+      for(int i = 0; i<num_commands; i++){
+	free(commands[i]);
+      }
+    
+
+
+}
+
+void execute_command(char *line){
+
+
+}
+
 int main(int arfc, char* argv[]){
   char line[MAX_INPUT_SIZE];
   char **tokens;
   int i;
 
-  const char *commands[] = {"ls", "cd", "pwd", "cat", "ps", "echo", "wc", "top", "grep", "sleep", "exit", NULL}; //Noticed that without NULL terminator I would get a Segmentation fault
 
-  void (*command_funct[])(char **) = {f_ls, f_cd, f_pwd, f_cat, f_ps, f_echo, f_wc, f_top, f_grep, f_sleep, f_exit};
+  const char *commands[] = {"ls", "cd", "pwd", "cat", "ps", "echo", "wc", "top", "grep", "sleep", "exit", NULL};
+  void (*command_funct[])(char **) = {execute_command, f_cd, execute_command, exexcute_command, execute_command, execute_command, execute_command, execute_command, execute_command, execute_command, f_exit};
   
   while(1){
 
     /*BEGIN: TAKING INPUT */
     bzero(line, sizeof(line));
     printf("$ ");
-    scanf("%[^\n]", line);
+    scanf("%[^\n]", line)
     getchar();
+    /* END: TAKING INPUT */
 
-    //printf("Command entered: %s (remove this debug output later)\n", line);
-      /* END: TAKING INPUT */
+    //if there is a '|' pipe
+    char *pipe_char = strchr(line, '|');
+    if (pipe_char != NULL){
+      execute_pipeline(line);
+    } else {
     
-    line[strlen(line)] = '\n'; //terminate wi
-    tokens = tokenize(line);
+      line[strlen(line)] = '\n';
+      tokens = tokenize(line);
 
-    //do what the commands, here we just print them
-    for(i=0; commands[i]!=NULL;i++){
-      if(strcmp(tokens[0], commands[i]) == 0){
-	command_funct[i](tokens); //call corresponding function
+      for(i=0; commands[i]!=NULL;i++){
+	if(strcmp(tokens[0], commands[i]) == 0){
+	  command_funct[i](tokens); /*call corresponding function. Notice that execute_command is called for the commands that require fork(), execvp() and or redirection. It will also handle redirection*/
+	}        
       }
-      //printf("found token %s (remove this debug output later)\n", token[i]);
+
     }
     
     
